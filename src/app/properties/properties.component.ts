aa6a3a645b69fd2f606d7f43855c2b16117110ed
Jonatan Michiels <michielsjonatan@gmail.com>
2020-01-02 11:09:39 +0100

    add mock api

--- src/app/properties/properties.component.ts
+++ src/app/properties/properties.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.css']
+  styleUrls: ['./properties.component.css'],
 })
 export class PropertiesComponent implements OnInit {
+  properties$ = this.http.get('/api/properties');
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/properties/properties.module.ts
+++ src/app/properties/properties.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { PropertiesRoutingModule } from './properties-routing.module';
 import { PropertiesComponent } from './properties.component';
 
-
 @NgModule({
   declarations: [PropertiesComponent],
-  imports: [
-    CommonModule,
-    PropertiesRoutingModule
-  ]
+  imports: [CommonModule, PropertiesRoutingModule, HttpClientModule],
 })
-export class PropertiesModule { }
+export class PropertiesModule {}
