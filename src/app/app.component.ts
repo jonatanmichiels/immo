acc7f4d9bd9060668e7937d86f219486e11461c7
Jonatan Michiels <michielsjonatan@gmail.com>
2020-01-17 12:42:55 +0100

    add guard + refactors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,5 @@
 import { Component } from '@angular/core';
-import {
-  OAuthService,
-  NullValidationHandler,
-  AuthConfig,
-} from 'angular-oauth2-oidc';
-
-import { issuer, clientId } from 'auth.conf.json';
+import { AuthService } from './auth-service';
 
 @Component({
   selector: 'app-root',
@@ -15,41 +9,7 @@ import { issuer, clientId } from 'auth.conf.json';
 export class AppComponent {
   title = 'Immo';
 
-  constructor(private oauthService: OAuthService) {
-    this.configure();
-  }
-
-  public login() {
-    this.oauthService.initLoginFlow();
-  }
-
-  public logout() {
-    this.oauthService.logOut();
-  }
-
-  public get name() {
-    const claims = this.oauthService.getIdentityClaims();
-    if (!claims) {
-      return null;
-    }
-    return claims;
-  }
-
-  private configure() {
-    const authConfig: AuthConfig = {
-      issuer,
-      clientId,
-      redirectUri: window.location.origin,
-      responseType: 'code',
-      scope: 'openid profile email offline_access api',
-      showDebugInformation: true,
-      disableAtHashCheck: true,
-    };
-
-    this.oauthService.configure(authConfig);
-    this.oauthService.tokenValidationHandler = new NullValidationHandler();
-    this.oauthService.loadDiscoveryDocumentAndTryLogin();
-    // this.oauthService.refreshToken();
-    this.oauthService.setupAutomaticSilentRefresh();
+  constructor(public auth: AuthService) {
+    this.auth.setup();
   }
 }
---
+++ src/app/auth-service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+  OAuthService,
+  AuthConfig,
+  NullValidationHandler,
+} from 'angular-oauth2-oidc';
+import { map } from 'rxjs/operators';
+import { issuer, clientId } from 'auth.conf.json';
+import { debug } from 'util';
+
+const authConfig: AuthConfig = {
+  issuer,
+  clientId,
+  redirectUri: window.location.origin,
+  responseType: 'code',
+  scope: 'openid profile email offline_access api',
+  showDebugInformation: true,
+  disableAtHashCheck: true,
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user$ = this.oauthService.events.pipe(map(() => this.getClaims()));
+  isAuthenticated$ = this.user$.pipe(map(() => this.hasValidIdToken()));
+
+  constructor(private oauthService: OAuthService, private router: Router) {}
+
+  setup() {
+    this.oauthService.configure(authConfig);
+    this.oauthService.tokenValidationHandler = new NullValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+    this.oauthService.setupAutomaticSilentRefresh();
+  }
+
+  login(targetUrl: string = this.router.url) {
+    this.oauthService.initCodeFlow(targetUrl);
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  hasValidIdToken() {
+    return this.oauthService.hasValidIdToken();
+  }
+
+  getClaims() {
+    return this.oauthService.getIdentityClaims();
+  }
+}
