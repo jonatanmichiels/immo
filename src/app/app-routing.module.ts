b8ad9737ac85861d56ce4ba8d8114684d7c65489
Jonatan Michiels <michielsjonatan@gmail.com>
2020-02-04 20:08:36 +0100

    authorize property call + protect routes by guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   {
     path: 'properties',
     loadChildren: () =>
       import('./properties/properties.module').then(m => m.PropertiesModule),
+    canActivate: [AuthGuard],
   },
   {
     path: 'profile',
     loadChildren: () =>
       import('./profile/profile.module').then(m => m.ProfileModule),
+    canActivate: [AuthGuard],
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { /* HTTP_INTERCEPTORS, */ HttpClientModule } from '@angular/common/http';
 import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { BearerTokenInterceptor } from './bearer-token-interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,8 +13,20 @@ import { AppComponent } from './app.component';
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
-    OAuthModule.forRoot()
+    OAuthModule.forRoot({
+      resourceServer: {
+        allowedUrls: ['https://immo-keycloak.meys.io'],
+        sendAccessToken: true,
+      },
+    }),
   ],
   bootstrap: [AppComponent],
+  // providers: [
+  //   {
+  //     provide: HTTP_INTERCEPTORS,
+  //     useClass: BearerTokenInterceptor,
+  //     multi: true,
+  //   },
+  // ],
 })
 export class AppModule {}
